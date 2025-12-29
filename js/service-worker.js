5da4a1417666b0a7ffac146bb13ff1b61ba4893e
Ewan Blane <45429298+Blane3011@users.noreply.github.com>
2025-12-29 01:43:38 +0000

    Updated serviceWorker

--- js/service-worker.js
+++ js/service-worker.js
@@ -6,13 +6,18 @@ self.addEventListener("install", (event) => {
     );
 });
  
-self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
-});
+self.addEventListener("fetch", event => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+  });
 
 var assets = [
     '/',
